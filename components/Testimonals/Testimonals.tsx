74e5c85aef30ce9d781f8676440d6af8f16aa5bc
piratefresh <magnussithnilsen@gmail.com>
2021-05-10 04:12:05 -0400

    Update Testimonals.tsx

--- components/Testimonals/Testimonals.tsx
+++ components/Testimonals/Testimonals.tsx
@@ -5,7 +5,7 @@ import s from "./Testimonals.module.css";
 import { testimonals } from "./TestimonalData";
 
 export default function Testimonals() {
-  const { isTabletOrMobile } = useIsTouchScreen();
+  const { isDesktopOrLaptop } = useIsTouchScreen();
   return (
     <div
       className="p-4 my-8 rounded-2xl"
@@ -15,26 +15,48 @@ export default function Testimonals() {
         <div className="text-4xl font-bold text-blue-400">Testimonals</div>
         <p className="">Our Reviews</p>
       </div>
-      <Masonry
-        breakpointCols={isTabletOrMobile ? 1 : 3}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        {testimonals.map((testimonal, index: number) => {
-          return (
-            <TestimonalCard key={index}>
-              <div className={s.testimonalText}>
-                <span className="font-bold text-sm md:text-2xl">"</span>
-                {testimonal.review}
-                <span className="font-bold text-sm md:text-2xl">"</span>
+      {isDesktopOrLaptop ? (
+        <Masonry
+          breakpointCols={3}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {testimonals.map((testimonal, index: number) => {
+            return (
+              <TestimonalCard>
+                <div className={s.testimonalText}>
+                  <span className="font-bold text-sm md:text-2xl">"</span>
+                  {testimonal.review}
+                  <span className="font-bold text-sm md:text-2xl">"</span>
+                </div>
+                <div className="flex flex-col font-semibold">
+                  {testimonal.name}
+                </div>
+              </TestimonalCard>
+            );
+          })}
+        </Masonry>
+      ) : (
+        <div className="flex flex-col">
+          {testimonals.map((testimonal, index: number) => {
+            return (
+              <div className="my-4" key={index}>
+                <TestimonalCard key={index}>
+                  <div className={s.testimonalText}>
+                    <span className="font-bold text-sm md:text-2xl">"</span>
+                    {testimonal.review}
+                    <span className="font-bold text-sm md:text-2xl">"</span>
+                  </div>
+                  <div className="flex flex-col font-semibold">
+                    {testimonal.name}
+                  </div>
+                </TestimonalCard>
               </div>
-              <div className="flex flex-col font-semibold">
-                {testimonal.name}
-              </div>
-            </TestimonalCard>
-          );
-        })}
-      </Masonry>
+            );
+          })}
+        </div>
+      )}
+
       <div className="flex">
         <button className="bg-blue-500 rounded-xl p-2 text-white mt-8 text-lg mx-auto">
           <a href="/testimonals">Read more..</a>
